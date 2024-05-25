52dbd38ef7a37b5d2cc897974407f3613e94204b
MinseokYu <alstjr6211@gmail.com>
2024-05-25 21:25:58 +0900

    update: colors

---
+++ lib/constant/font.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+TextStyle GmarketSans8 = const TextStyle(
+  fontFamily: "GmarketSans",
+  color: Colors.black,
+  fontSize: 8,
+  fontWeight: FontWeight.bold,
+);
+TextStyle GmarketSans12 = const TextStyle(
+  fontFamily: "GmarketSans",
+  color: Colors.black,
+  fontSize: 12,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle GmarketSans24 = const TextStyle(
+  fontFamily: "GmarketSans",
+  color: Colors.black,
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
