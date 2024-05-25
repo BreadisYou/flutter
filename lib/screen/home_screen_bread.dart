ae2955e4e8f968cd25972d1d7631825db4c028c7
MinseokYu <alstjr6211@gmail.com>
2024-05-25 23:15:30 +0900

    home fin

--- lib/screen/home_screen_bread.dart
+++ lib/screen/home_screen_bread.dart
@@ -26,17 +26,17 @@ class _HomeScreenBreadState extends State<HomeScreenBread> {
         .of(context)
         .size
         .width;
-
     return Scaffold(
-      backgroundColor: colorApricot,
+      backgroundColor: colorApricotWhite,
       body: SafeArea(
         child: Container(
-          color: colorApricot,
+          color: colorApricotWhite,
           height: _screenheight,
           width: _screenwidth,
           child: Padding(
-            padding: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(15),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
@@ -50,13 +50,13 @@ class _HomeScreenBreadState extends State<HomeScreenBread> {
                 ),
                 SizedBox(
                   height: _screenwidth * 0.33,
-                  width: _screenwidth - 20,
+                  width: _screenwidth - 30,
                   child: Stack(
                     children: [
                       Positioned(
                         child: Container(
                           height: _screenheight * 0.33,
-                          width: _screenwidth - 20, //TODO firebase에서 Image 가져와야함
+                          width: _screenwidth - 30, //TODO firebase에서 Image 가져와야함
                           child: Image.asset(
                             'assets/image/firebase_image.png',
                             fit: BoxFit.cover,
@@ -68,7 +68,7 @@ class _HomeScreenBreadState extends State<HomeScreenBread> {
                         left: 0,
                         child: Container(
                           height: _screenheight * 0.07,
-                          width: _screenwidth - 20,
+                          width: _screenwidth - 30,
                           color: Colors.black..withOpacity(0.5),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
@@ -102,6 +102,40 @@ class _HomeScreenBreadState extends State<HomeScreenBread> {
                       ),
                     ],
                   ),
+                ),
+                Container(
+                  height: _screenheight * 0.11,
+                  width: _screenwidth - 30,
+                  color: colorLightBrown,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/review.png',
+                        width: 20,
+                        height: 20,
+                      ),
+                      Text(
+                        "리뷰하기",
+                        style: GmarketSans12,
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: _screenheight * 0.22,
+                      width: _screenwidth / 2 - 30,
+                      color: const Color(0xffFFEFB5),
+                    ),
+                    Container(
+                      height: _screenheight * 0.22,
+                      width: _screenwidth / 2 - 30,
+                      color: const Color(0xffDBFFBE),
+                    )
+                  ],
                 )
               ],
             ),
