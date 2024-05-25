f4f101d5dbdee05ce35cbf65d69bde27c9a8628b
awj1052 <awj1052@naver.com>
2024-05-25 23:16:29 +0900

    test

--- lib/screen/home_screen_desert.dart
+++ lib/screen/home_screen_desert.dart
@@ -4,6 +4,7 @@ import 'package:sogra/common/logo.dart';
 import 'package:sogra/constant/colors.dart';
 import 'package:sogra/constant/font.dart';
 import 'package:sogra/main.dart';
+import 'package:sogra/screen/home_screen_bread.dart';
 import 'package:sogra/widget/image_slider.dart';
 
 class HomeScreenDesert extends StatefulWidget {
@@ -50,47 +51,62 @@ class _HomeScreenDesertState extends State<HomeScreenDesert> {
                   "이번 주 추천 빵",
                   style: GmarketSans24,
                 ),
-                Stack(
-                  children: [
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      child: Container(
-                        height: _screenheight * 0.33,
-                        width: _screenwidth - 20, //TODO firebase에서 Image 가져와야함
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      left: 0,
-                      child: Container(
-                        width: _screenwidth - 20,
-                        height: _screenheight * 0.07,
-                        color: Colors.black..withOpacity(0.5),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "구독하기",
-                              style: GmarketSans12,
-                            ),
-                          ],
+                SizedBox(
+                  height: _screenwidth * 0.33,
+                  width: _screenwidth - 20,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        child: Container(
+                          height: _screenheight * 0.33,
+                          width: _screenwidth - 20, //TODO firebase에서 Image 가져와야함
+                          child: Image.network( // 임시 이미지 링크
+                            'https://firebasestorage.googleapis.com/v0/b/sogra-4d8b4.appspot.com/o/dessert_2024_4.png?alt=media&token=a6ce75d6-101f-4f70-926d-25c82c120452',
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      top: 10,
-                      right : 0,
-                      child: ElevatedButton(
-                        onPressed: (){}, //TODO Navigtor To HomeScreenBread()
-                        child: Image.asset(
-                          'assets/image/bread.png',
-                          width: 100,
-                          height: 100,
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        child: Container(
+                          width: _screenwidth - 20,
+                          height: _screenheight * 0.07,
+                          color: Colors.black..withOpacity(0.5),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "구독하기",
+                                style: GmarketSans12,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                      Positioned(
+                        top: 10,
+                        right : 0,
+                        child: ElevatedButton(
+                          onPressed: (){
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                builder: (context) => HomeScreenBread(),
+                              ),
+                            );
+                          }, //TODO Navigtor To HomeScreenBread()
+                          child: Image.network(
+                            'https://firebasestorage.googleapis.com/v0/b/sogra-4d8b4.appspot.com/o/bread_button.png?alt=media&token=6ece360a-a51e-417d-a748-79f0dfffde37',
+                            width: 100,
+                            height: 100,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
